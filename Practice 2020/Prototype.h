d5216b274dab07c108552053c6f8f118931696b5
Dimushka <67047109+Dimushka@users.noreply.github.com>
2020-06-23 12:20:30 +0300

    Прототип

---
+++ Practice 2020/Prototype.h
@@ -0,0 +1,9 @@
+/* 
+/* @param x1 y1 координаты точки А отрезка AB
+/* @param x2 y2 координаты точки B отрезка  AB
+/* @param x3 y3 координаты точки C отрезка CD
+/* @param x4 y4 координаты точки D отрезка CD
+/* @return 0, если нет пересечения
+          1, если пересекаються
+*/
+int intersection(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4, float d);
