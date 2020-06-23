99a37c9d658bef332c80da75580e45c986a38689
Dimushka <67047109+Dimushka@users.noreply.github.com>
2020-06-23 12:23:32 +0300

    main()

--- Practice 2020/Practice 2020.cpp
+++ Practice 2020/Practice 2020.cpp
@@ -1,13 +1,7 @@
-﻿/**
-*@file СoncoleApplication1.cpp
-*@date 19 червня 2020 год
-*@author Фещук Д.Ю.
-*@brief Навчальна практика №1
-*
-**/
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
+#include "MyFunc.h"
 int main(void)
 {
 	setlocale(LC_CTYPE, "rus");
@@ -25,15 +19,4 @@ int main(void)
 	if (intersection_of_segments(Ax, Bx, Cx, Dx, Ay, By, Cy, Dy, det)) printf("Отрезки пересекаются!\n");
 	else printf("Отрезки не пересекаются!\n");
 	return 0;
-}
-
-int intersection_of_segments(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4, float d)
-{
-	float Ua, Ub;
-	if (d == 0)  return 0;
-	Ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / d;
-	Ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / d;
-	if (Ua >= 0 && Ua <= 1 && Ub >= 0 && Ub <= 1) return 1; //Условие пересечения
-
-	else return 0;
 }
